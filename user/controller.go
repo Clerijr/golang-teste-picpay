c4b6c19f0a9c093aee8efab7ab11517181916637
Clerivaldo <clerivaldoj@gmail.com>
2024-06-02 18:35:08 -0300

    feat: initiate user controller

---
+++ user/controller.go
@@ -0,0 +1,23 @@
+package user
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Controller struct {
+}
+
+func NewController() *Controller {
+	return &Controller{}
+}
+
+func (c *Controller) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("funfo")
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (c *Controller) Login(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("logou")
+	w.WriteHeader(http.StatusOK)
+}
